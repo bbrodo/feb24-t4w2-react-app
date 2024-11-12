9d33a34278bec21ed4ce946569b7d6d09cb298bf
bbrodo <15292@coderacademy.edu.au>
2024-11-12 15:14:48 +1100

    feat: user auth context implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useUserAuthContext } from "./contexts/UserAuthContextProvider";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button data-testid="counterButton" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [userJwt, setUserJwt] = useUserAuthContext();
+
+    useEffect(() => {
+        console.log(import.meta.env.VITE_AUTH_API_URL);
+    }, []);
+
+    const getProtectedRoute = async () => {
+        let response = await fetch(
+            `${import.meta.env.VITE_AUTH_API_URL}/protectedRoute`
+        );
+        let data = await response.json();
+        console.log(data);
+    };
+
+    const postUserSignUp = async () => {
+        let userDetails = {
+            username: "Brodey" + Math.floor(Math.random() * 1000),
+            password: "Password1",
+        };
+
+        let response = await fetch(
+            `${import.meta.env.VITE_AUTH_API_URL}/signup`,
+            {
+                method: "POST",
+                headers: {
+                  'Content-Type': "application/json"
+                },
+                body: JSON.stringify(userDetails),
+            }
+        );
+
+        let data = await response.json();
+        console.log(data);
+        setUserJwt(data.jwt);
+    };
+
+    return (
+          <>
+            <div>
+                <button onClick={postUserSignUp}>sign up user</button>
+                <button onClick={getProtectedRoute}>
+                    Visit Protected API Route
+                </button>
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img
+                        src={reactLogo}
+                        className="logo react"
+                        alt="React logo"
+                    />
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+                <button
+                    data-testid="counterButton"
+                    onClick={() => setCount((count) => count + 1)}
+                >
+                    count is {count}
+                </button>
+                <p>
+                    Edit <code>src/App.jsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </>
+    );
 }
 
-export default App
+export default App;
