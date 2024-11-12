64f3b60d37d3ae4a2222c53e07c1140621a30b0a
bbrodo <15292@coderacademy.edu.au>
2024-11-12 14:39:31 +1100

    feat: react test implemented

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -1,6 +1,7 @@
 import { expect, test } from "vitest";
 import App from "../src/App";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 test("render the app component", () => {
     render(<App />);
@@ -8,3 +9,18 @@ test("render the app component", () => {
     const mainPageHeader = screen.getByText("Vite + React");
     expect(mainPageHeader).toBeInTheDocument();
 });
+
+test("render the app component with a button that makes a number go up", async () => {
+    render(<App />);
+
+    let counterButton = screen.getByTestId("counterButton");
+
+    expect(counterButton).toBeInTheDocument();
+    expect(counterButton).toHaveTextContent("count is 0");
+
+    const user = userEvent.setup();
+
+    await user.click(counterButton);
+
+    expect(counterButton).toHaveTextContent("count is 1")
+})
